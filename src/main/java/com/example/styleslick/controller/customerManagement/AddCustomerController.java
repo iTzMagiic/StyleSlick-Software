fb8695e76c72b49a982d81bb1c554fb0e34e419a
Anton <anton-gruenwald@outlook.de>
2024-11-22 23:12:12 +0100

    Update

--- src/main/java/com/example/styleslick/controller/customerManagement/AddCustomerController.java
+++ src/main/java/com/example/styleslick/controller/customerManagement/AddCustomerController.java
@@ -2,13 +2,30 @@ package com.example.styleslick.controller.customerManagement;
 
 import com.example.styleslick.model.Database;
 import com.example.styleslick.model.UserSession;
+import com.example.styleslick.utils.SceneManager;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 
 public class AddCustomerController {
 
     UserSession userSession;
+
+    @FXML
+    private TextField field_username;
+    @FXML
+    private TextField field_name;
+    @FXML
+    private TextField field_lastName;
+    @FXML
+    private TextField field_street;
+    @FXML
+    private TextField field_plz;
+    @FXML
+    private TextField field_ort;
+    @FXML
+    private TextField field_platform;
 
 
 
@@ -26,13 +43,14 @@ public class AddCustomerController {
          */
 
 
-        String username = ""; // Textfield.getText();
-        String name = "";
-        String lastName = "";
-        String street = "";
-        int plz = 0;
-        String ort = "";
-        String platform = "";
+        String username = field_username.getText();
+        String name = field_name.getText();
+        String lastName = field_lastName.getText();
+        String street = field_street.getText();
+        String stringPlz = field_plz.getText();
+        int plz = Integer.parseInt(stringPlz);
+        String ort = field_ort.getText();
+        String platform = field_platform.getText();
 
         Database database = userSession.getDatabase();
 
@@ -49,6 +67,25 @@ public class AddCustomerController {
     @FXML
     private void onMouseClickedAddCustomer(MouseEvent event) {
         executeAddCustomer();
+    }
+
+
+
+    @FXML
+    private void executeExitAddCustomer() {
+        SceneManager.switchScene("/com/example/styleslick/loggedIn-view.fxml", "Willkommen")
+    }
+
+    @FXML
+    private void onKeyPressedEnterExitAddCustomer(KeyEvent event) {
+        if (event.getCode().toString().equals("ENTER")) {
+            executeExitAddCustomer();
+        }
+    }
+
+    @FXML
+    private void onMouseClickedExitAddCustomer(MouseEvent event) {
+        executeExitAddCustomer();
     }
 
 }
