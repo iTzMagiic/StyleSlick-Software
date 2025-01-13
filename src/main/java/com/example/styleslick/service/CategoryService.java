3dba1bb9d3fdd70355316e1dfa57ffc15d1eed11
Anton <anton-gruenwald@outlook.de>
2025-01-13 21:38:16 +0100

    Update CategoryService

--- src/main/java/com/example/styleslick/service/CategoryService.java
+++ src/main/java/com/example/styleslick/service/CategoryService.java
@@ -20,7 +20,6 @@ public class CategoryService {
 
     private static CategoryService categoryService;
     private Database database;
-    private static final Logger logger = LoggerFactory.getLogger(CategoryService.class);
 
 
     private CategoryService() {
@@ -39,7 +38,7 @@ public class CategoryService {
 
 
     public boolean addCategory(Map<String, String> fields) {
-        logger.info("Methode addCategory() START.");
+
         Map<String, String> filledFields = new HashMap<>();
 
         for (Map.Entry<String, String> entry : fields.entrySet()) {
@@ -50,24 +49,21 @@ public class CategoryService {
 
         if (filledFields.isEmpty()) {
             AlertService.showErrorAlert("Bitte tragen Sie was ein.");
-            logger.warn("Liste mit ausgefüllten Feldern ist leer ENDE.\n");
             return false;
         }
 
         if (!database.addCategory(filledFields)) {
             AlertService.showErrorAlert("Fehler beim erstellen.");
-            logger.warn("Fehler, Kategorie wurde nicht in die Datenbank geschieben ENDE.\n");
             return false;
         }
 
         AlertService.showConfirmAlert("Die Kategorie wurde erfolgreich erstellt.");
-        logger.info("Methode addCategory() erfolgreich ENDE.\n");
         return true;
     }
 
 
     public boolean updateCategory(Map<String, String> fields, int categoryID) {
-        logger.info("Methode updateCategory() START.");
+
         Map<String, String> filledFields = new HashMap<>();
 
         for (Map.Entry<String, String> entry : fields.entrySet()) {
@@ -78,58 +74,47 @@ public class CategoryService {
 
         if (filledFields.isEmpty()) {
             AlertService.showErrorAlert("Bitte geben Sie was ein um die Kategorie zu bearbeiten.");
-            logger.warn("Liste mit ausgefüllten Feldern ist leer ENDE\n");
             return false;
         }
 
         if (!AlertService.showConfirmAlertResult("Möchten Sie wirklich die Kategorie mit der Kategorie-Nr " + categoryID + " bearbeiten?")) {
             AlertService.showErrorAlert("Artikel wird nicht bearbeitet.");
-            logger.debug("Kategorie bearbeitung wurde durch den Benutzer beendet ENDE.\n");
             return false;
         }
 
         if (!database.updateCategory(filledFields, categoryID)) {
             AlertService.showErrorAlert("Fehler beim bearbeiten der Kategorie.");
-            logger.warn("Kategorie wurde nicht in die Datenbank geschrieben ENDE.\n");
             return false;
         }
 
-        logger.info("Methode updateCategory() erfolgreich ENDE.\n");
         return true;
     }
 
 
     public boolean deleteCategory(int categoryID) {
-        logger.info("Methode deleteCategory() START.");
 
         if (!AlertService.showConfirmAlertResult("Möchten Sie wirklich die Kategorie mit der Kategorie-Nr '" + categoryID + "' löschen?")) {
             AlertService.showErrorAlert("Kategorie wird nicht gelöscht.");
-            logger.debug("Kategorie löschen wurde durch Benutzer beendet ENDE.\n");
             return false;
         }
 
         if (!database.deleteCategory(categoryID)) {
             AlertService.showErrorAlert("Fehler beim löschen der Kategorie.");
-            logger.warn("Kategorie wurde nicht in der Datenbank gelöscht ENDE.\n");
             return false;
         }
 
         AlertService.showConfirmAlert("Kategorie wurde erfolgreich gelöscht.");
-        logger.info("Methode deleteCategory() erfolgreich ENDE.\n");
         return true;
     }
 
 
     public List<Category> getAllCategories() {
-        logger.info("Methode getAllCategories() START.");
         List<Category> listOfCategories = database.getAllCategories();
         if (listOfCategories == null || listOfCategories.isEmpty()) {
             AlertService.showErrorAlert("Fehler beim Laden der Kategorien.");
-            logger.warn("Fehler beim Laden der Kategorien Liste ist leer ENDE.\n");
             return listOfCategories;
         }
 
-        logger.info("Methode getAllCategories() erfolgreich ENDE.\n");
         return listOfCategories;
     }
 
