ffb45ee8db69f0fc101d5d534f4865107746bb1c
Anton <anton-gruenwald@outlook.de>
2025-01-16 21:34:53 +0100

    Update ArticleManagement-view

--- src/main/java/com/example/styleslick/controller/InvoiceManagementController.java
+++ src/main/java/com/example/styleslick/controller/InvoiceManagementController.java
@@ -4,27 +4,41 @@ import com.example.styleslick.utils.SceneManager;
 import javafx.fxml.FXML;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
-import javafx.fxml.FXML;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.input.MouseEvent;
 
-public class InvoiceManagementManagementController {
+public class InvoiceManagementController {
+
+
+    @FXML
+    private void executeShowAllInvoices() {
+
+    }
 
 
     @FXML
     private void executeExitInvoiceManagement(){
         SceneManager.switchScene("/com/example/styleslick/loggedIn-view.fxml", "Willkommen");
     }
+
+
+
+    @FXML
+    private void onKeyPressedEnterShowAllInvoices(KeyEvent event) {
+        if (event.getCode().toString().equals("ENTER")) {
+            executeShowAllInvoices();
+        }
+    }
+
     @FXML
     private void onKeyPressedEnterExitInvoiceManagement(KeyEvent event){
         if(event.getCode().toString().equals("ENTER")){
             executeExitInvoiceManagement();
         }
+    }
+
+
+    @FXML
+    private void onMouseClickedShowAllInvoices(MouseEvent event) {
+        executeShowAllInvoices();
     }
     @FXML
     private void onMouseClickedExitInvoiceManagement(MouseEvent event){
